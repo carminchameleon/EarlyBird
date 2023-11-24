e320fa15bcf54b6af4a6589d5f5fe54dac6ae8c1
carminchameleon <hwanggee1211@gmail.com>
2023-11-25 10:23:28 +1100

    Initial Commit

---
+++ EarlyBird/EarlyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarlyBirdApp.swift
+//  EarlyBird
+//
+//  Created by Eunji Hwang on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EarlyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
